359a19a70f749dbb2ab7b74929d0f16bc8a79976
Rafael <rafaelromanrezende123@gmail.com>
2026-09-10 14:39:31 -0300

    add cabeçalho

--- aula2/modelo/tests.py
+++ aula2/modelo/tests.py
@@ -1,3 +1,9 @@
+#Eduardo Gobbi Jorge | NUSP: 5980278
+#Marcelo Olivetti França Neri de Almeida | NUSP: 15474654
+#Rafael Romanello Rezende | NUSP: 15485490
+#Vinícius Akira Durante Tahara | NUSP: 12547002
+
+
 import unittest
 
 class TestModelo(unittest.TestCase):
